1bf11ae2320788871025ffa59b5eaad1ca65acfa
pretor <3nity.pretor@gmail.com>
2015-02-07 23:31:18 +0100

    add more info

--- __main__.py
+++ __main__.py
@@ -1,13 +1,20 @@
 import psutil
 import requests
+import platform
+from cpuinfo import cpuinfo
 
+info = cpuinfo.get_cpu_info()
 coresLoadPercentage = psutil.cpu_percent(interval=1, percpu=True)
+memoryInfo = psutil.virtual_memory()
 
 payload = {}
 
 for x in range(0,len(coresLoadPercentage)):	
 	payload['core'+str(x)] = coresLoadPercentage[x]
 
-r = requests.get("http://pretor.pythonanywhere.com/get", params=payload)
+tes = {'cpu[]'    : coresLoadPercentage,
+       'cpubrand' : info['brand'],
+       'memorypercent'   : memoryInfo.percent
+}
 
-
+r = requests.get("http://127.0.0.1:8000/get", params=tes)
